8cd25770b65567d1c45850246bbf744449a93e0b
Michal Gajda <michal@gajda.co.uk>
2023-06-26 13:09:03 +0100

    Remove comments

--- Nuntius/src/Gajda.Nuntius.Worker/Program.cs
+++ Nuntius/src/Gajda.Nuntius.Worker/Program.cs
@@ -1,12 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-// builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
-
 builder.Host.UseOrleans(siloBuilder =>
 {
     siloBuilder.UseLocalhostClustering();
@@ -14,29 +7,12 @@ builder.Host.UseOrleans(siloBuilder =>
 
     siloBuilder.UseDashboard(options =>
     {
-        // options.Username = "admin";
-        // options.Password = "admin";
-        // options.Host = "*";
         options.Port = 8080;
         options.HostSelf = true;
         options.CounterUpdateIntervalMs = 5000;
     });
-    // siloBuilder.AddMemoryGrainStorage("urls");
 });
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    // app.UseSwagger();
-    // app.UseSwaggerUI();
-}
-
-// app.UseHttpsRedirection();
-
-// app.UseAuthorization();
-
-// app.MapControllers();
 
 await app.RunAsync();
--- Recipiens/src/Gajda.Recipiens.Web/Program.cs
+++ Recipiens/src/Gajda.Recipiens.Web/Program.cs
@@ -1,9 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -11,7 +8,6 @@ builder.Services.AddOrleansClient(clientBuilder => clientBuilder.UseLocalhostClu
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -24,4 +20,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+await app.RunAsync();
